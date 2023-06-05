45cae2e0fe9a09a068ca44ff54893bb2e287b238
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2023-06-05 20:33:10 +0200

    refactor: minor

--- db.py
+++ db.py
@@ -5,12 +5,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALGORITHM = os.environ.get('ALGORITHM')
-
 SQLALCHEMY_DATABASE_URL = os.environ.get('POSTGRES')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
